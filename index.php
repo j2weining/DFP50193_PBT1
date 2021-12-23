65746fb23412574d03c611d769af8888974cb691
CHAI WEI NING <weining2001@gmail.com>
2021-12-23 09:09:14 +0800

    pbt1

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hair Garden Salon</title>
+</head>
+<body>
+    <h1>Hair Garden Salon</h1>
+    <p></p>
+    <form method="post" action="function.php">
+        <table border="1">
+            <tr>
+                <td>Services</td>
+                <td>Price</td>
+                <td>Additional Services</td>
+                <td>Price</td>
+            </tr>
+            <tr>
+                <td>Hair Cut</td>
+                <td>RM 10.00</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Hair Wash</td>
+                <td>RM 5.00</td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
