8002c11a4f15be90d6cda10d653167acb30a42f5
CHAI WEI NING <weining2001@gmail.com>
2021-12-25 01:38:35 +0800

    pbt1

--- function.php
+++ function.php
@@ -2,26 +2,27 @@
 $service = $_POST['service'];
 $addservice = $_POST['addservice'];
 
-function totalprice($total){
+function totalprice($total)
+{
     $service = $_POST['service'];
     $addservice = $_POST['addservice'];
-    if($service == 'haircut'){
-        if($addservice == 'wash'){
-            $total =+ 15;
-        }else{
-            $total =+ 10;
+    if ($service == 'haircut') {
+        if ($addservice == 'wash') {
+            $total = +15;
+        } else {
+            $total = +10;
         }
-    }elseif ($service == 'hairtreatment'){
-        if($addservice == 'moisture'){
-            $total =+ 25;
-        }elseif($addservice == 'relax'){
-            $total =+ 25;
-        }else{
-            $total =+ 20;
+    } elseif ($service == 'hairtreatment') {
+        if ($addservice == 'moisture') {
+            $total = +25;
+        } elseif ($addservice == 'relax') {
+            $total = +25;
+        } else {
+            $total = +20;
         }
-    }elseif ($service == 'hairdye'){
-        $total =+ 50;
-    }else{
+    } elseif ($service == 'hairdye') {
+        $total = +50;
+    } else {
         $total = 0;
     }
     return $total;
--- result.php
+++ result.php
@@ -28,31 +28,31 @@ $total = totalprice($_POST);
             <tr>
                 <td>Hair Cut</td>
                 <td>RM 10.00</td>
-                <td><input type="radio" name="service" value="haircut" <?php if($service == 'haircut') echo " checked";?>></td>
+                <td><input type="radio" name="service" value="haircut" <?php if ($service == 'haircut') echo " checked"; ?>></td>
                 <td>Hair Wash</td>
                 <td>RM 5.00</td>
-                <td><input type="checkbox" name="addservice" value="wash" <?php if($addservice == 'wash') echo " checked";?>></td>
+                <td><input type="checkbox" name="addservice" value="wash" <?php if ($addservice == 'wash') echo " checked"; ?>></td>
             </tr>
             <tr>
                 <td rowspan="2">Hair Treatment</td>
                 <td rowspan="2">RM 20.00</td>
-                <td rowspan="2"><input type="radio" name="service" value="hairtreatment" <?php if($service == 'hairtreatment') echo " checked";?>></td>
+                <td rowspan="2"><input type="radio" name="service" value="hairtreatment" <?php if ($service == 'hairtreatment') echo " checked"; ?>></td>
                 <td>Moisture Treatment</td>
                 <td>RM 5.00</td>
-                <td><input type="checkbox" name="addservice" value="moisture" <?php if($addservice == 'moisture') echo " checked";?>></td>
+                <td><input type="checkbox" name="addservice" value="moisture" <?php if ($addservice == 'moisture') echo " checked"; ?>></td>
             </tr>
             <tr>
                 <td>Relax Treatment</td>
                 <td>RM 5.00</td>
-                <td><input type="checkbox" name="addservice" value="relax" <?php if($addservice == 'relax') echo " checked";?>></td>
+                <td><input type="checkbox" name="addservice" value="relax" <?php if ($addservice == 'relax') echo " checked"; ?>></td>
             </tr>
             <tr>
                 <td>Hair Dye</td>
                 <td>RM 50.00</td>
-                <td><input type="radio" name="service" value="hairdye" <?php if($service == 'hairdye') echo " checked";?>></td>
+                <td><input type="radio" name="service" value="hairdye" <?php if ($service == 'hairdye') echo " checked"; ?>></td>
                 <td>Hair Bleach</td>
                 <td>RM 20.00</td>
-                <td><input type="checkbox" name="addservice" value="hairbleach" <?php if($addservice == 'hairbleach') echo " checked";?>></td>
+                <td><input type="checkbox" name="addservice" value="hairbleach" <?php if ($addservice == 'hairbleach') echo " checked"; ?>></td>
             </tr>
             <tr>
                 <td colspan="6">Total Price</td>
